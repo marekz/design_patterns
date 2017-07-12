81d076a3730c9ce33ee52c75f3ad001ae59a2a8b
Marek Zdybel <marek@motta.com.pl>
2017-07-12 23:20:47 +0200

    Singleton

---
+++ Singleton/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<pre>";
+include_once './MySQLManager.php';
+$a = new MySQLManager();
+$b = new MySQLManager();
+$c = new MySQLManager();
+$d = new MySQLManager();
+$e = new MySQLManager();
+echo "</pre>";
