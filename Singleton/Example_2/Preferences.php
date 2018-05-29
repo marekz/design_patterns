f7d60017495e5b455a32850ac7a8d4847e33f445
Marek Zdybel <marek@motta.com.pl>
2018-05-29 06:49:31 +0200

    Another PHP singleton

---
+++ Singleton/Example_2/Preferences.php
@@ -0,0 +1,36 @@
+<?php
+
+class Preferences {
+
+    private $props = [];
+    private static $instance;
+
+    private function __construct() {
+	
+    }
+    
+    public static function getInstance(){
+	if(empty(self::$instance)){
+	    self::$instance = new Preferences();
+	}
+	
+	return self::$instance;
+    }
+
+    public function setProperty(string $key, string $val) {
+	$this->props[$key] = $val;
+    }
+
+    public function getProperty(string $key) {
+	return $this->props[$key];
+    }
+
+}
+
+$pref = Preferences::getInstance();
+$pref->setProperty("imie", "Matt");
+
+unset($pref);
+
+$pref2 = Preferences::getInstance();
+print $pref2->getProperty("imie") . "\n";
